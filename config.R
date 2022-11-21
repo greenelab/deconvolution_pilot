90c273b98b323b667edf2d5c7771afde43c2235a
arielah <ariel.hippen@gmail.com>
2022-11-20 20:27:20 -0500

    Removed old config file

--- config.R
+++
@@ -1,3 +0,0 @@
-data_path <- normalizePath("~/Documents/scRNA/sc-cancer-hgsc/data")
-local_data_path <- normalizePath("~/Documents/scRNA/deconvolution_pilot/data")
-samples <- c("2251", "2267", "2283", "2293", "2380", "2428", "2467", "2497")
